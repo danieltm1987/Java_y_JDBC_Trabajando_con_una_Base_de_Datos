b16dc1c8773214ed34ec0185873c07a3b8aca6af
Daniel Torres Martinez <daniel.tm1987@gmail.com>
2023-05-07 23:22:21 -0500

    commit 02: Sea agrega el proyecto entregado en la unidad 2

---
+++ src/main/java/com/alura/jdbc/controller/ProductoController.java
@@ -0,0 +1,70 @@
+package com.alura.jdbc.controller;
+
+import java.sql.Statement;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.alura.jdbc.factory.ConnectionFactory;
+
+public class ProductoController {
+
+	public void modificar(String nombre, String descripcion, Integer id) {
+		// TODO
+	}
+
+	public void eliminar(Integer id) {
+		// TODO
+	}
+
+	public List<Map<String, String>> listar() throws SQLException {
+		
+		Connection con = new ConnectionFactory().recuperaConexion();
+		
+		Statement statement = con.createStatement();
+		
+		statement.execute("SELECT ID, NOMBRE, DESCRIPCION, CANTIDAD FROM PRODUCTO");
+		
+		ResultSet resultSet = statement.getResultSet();
+		
+		List<Map<String, String>> resultado = new ArrayList<>();
+		
+		while(resultSet.next()) {
+			Map<String, String> fila = new HashMap<>();
+			fila.put("ID", String.valueOf(resultSet.getInt("ID")));
+			fila.put("NOMBRE", resultSet.getString("NOMBRE"));
+			fila.put("DESCRIPCION", resultSet.getString("DESCRIPCION"));
+			fila.put("CANTIDAD", String.valueOf(resultSet.getInt("CANTIDAD")));
+			
+			resultado.add(fila);
+		}
+
+		con.close();		
+		
+		return resultado;
+	}
+
+    public void guardar(Map<String, String> producto) throws SQLException {
+    	Connection con = new ConnectionFactory().recuperaConexion();
+    	Statement statement = con.createStatement();
+    	
+    	statement.execute("INSERT INTO PRODUCTO(NOMBRE, DESCRIPCION, CANTIDAD) "
+    			+ " VALUES('"+ producto.get("NOMBRE")+"', '"
+    			+ producto.get("DESCRIPCION")+"',"
+    			+ producto.get("CANTIDAD")+")", Statement.RETURN_GENERATED_KEYS);
+    			
+    	ResultSet resulSet = statement.getGeneratedKeys();
+    	
+    	while(resulSet.next()) {
+    		System.out.println(
+    				String.format(
+    						"Fue insertado el producto de ID %d",
+    						resulSet.getInt(1)));
+    	}
+	}
+    
+}
