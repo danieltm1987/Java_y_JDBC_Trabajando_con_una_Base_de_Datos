9092178d8c99e3e8fd8eafa4562c6a84e6e3018d
Daniel Torres Martinez <daniel.tm1987@gmail.com>
2023-05-18 00:22:10 -0500

    commit 22: optimizar las consultas a la BD usando INNER JOIN.

--- src/main/java/com/alura/jdbc/dao/CategoriaDAO.java
+++ src/main/java/com/alura/jdbc/dao/CategoriaDAO.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.alura.jdbc.modelo.Categoria;
+import com.alura.jdbc.modelo.Producto;
 import com.mysql.cj.protocol.Resultset;
 
 public class CategoriaDAO {
@@ -36,6 +37,49 @@ public class CategoriaDAO {
 								resultSet.getString("NOMBRE"));
 						
 						resultado.add(categoria);						
+					}
+				}
+			}
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+		
+		return resultado;
+	}
+
+	public List<Categoria> listarConProductos() {
+List<Categoria> resultado = new ArrayList<>();
+		
+		try {
+			var querySelect = "SELECT C.ID, C.NOMBRE, P.ID, P.NOMBRE, P.CANTIDAD "
+					+ "FROM CATEGORIA C "
+					+ "INNER JOIN PRODUCTO P ON C.ID = P.CATEGORIA_ID";
+			System.out.println(querySelect);
+			
+			final PreparedStatement statement = con.prepareStatement(querySelect);
+			
+			try(statement) {
+				final ResultSet resultSet = statement.executeQuery();
+				
+				try(resultSet) {
+					while(resultSet.next()) {
+						int categoriaId = resultSet.getInt("ID"); 
+						String categoriaNombre = resultSet.getString("NOMBRE");
+						
+						var categoria = resultado
+								.stream()
+								.filter(cat -> cat.getId().equals(categoriaId))
+								.findAny().orElseGet(() -> {
+									Categoria cat = new Categoria(categoriaId, categoriaNombre);
+									resultado.add(cat);
+									return cat;
+								});
+						Producto producto = new Producto(
+								resultSet.getInt("P.ID"),
+								resultSet.getString("P.NOMBRE"),
+								resultSet.getInt("P.CANTIDAD"));
+						
+						categoria.agregar(producto);
 					}
 				}
 			}
--- src/main/java/com/alura/jdbc/modelo/Categoria.java
+++ src/main/java/com/alura/jdbc/modelo/Categoria.java
@@ -1,9 +1,13 @@
 package com.alura.jdbc.modelo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Categoria {	
 
 	private Integer id;
 	private String nombre;
+	private List<Producto> productos;
 	
 	public Categoria(int id, String nombre) {
 		this.id = id;
@@ -14,14 +18,21 @@ public class Categoria {
 		return this.id;
 	}
 	
+	public void agregar(Producto producto) {
+		if(this.productos == null) {
+			this.productos = new ArrayList<>();			
+		}
+		
+		productos.add(producto);
+	}
+	
+	public List<Producto>  getProductos() {
+		return this.productos;
+	}
+	
 	@Override
 	public String toString() {
 	 	return this.nombre;
 	}
-
-	
-	
-	 
-	
 
 }
